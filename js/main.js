a87a4384d1e1f735c46e47d1844d1b7a42e7df17
Kevin <GreenApple76@users.noreply.github.com>
2018-02-11 16:05:59 -0500

    add jump to links and fix overscrolling

--- js/main.js
+++ js/main.js
@@ -4,4 +4,9 @@ $(document).ready(function() {
         console.log('navbar icon clicked');
         $('.logo').toggle(); // should logo be shown or hidden
     });
+
+    // prevent jump to links from scrolling past headline
+    window.addEventListener('hashchange', function() {
+        window.scrollTo(window.scrollX, window.scrollY - 80);
+    });
 });
