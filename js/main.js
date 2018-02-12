8af6eafa1378e2c997da0cb224097a36b617c559
Kevin <GreenApple76@users.noreply.github.com>
2018-02-12 13:07:02 -0500

    remove logo toggling

--- js/main.js
+++ js/main.js
@@ -1,10 +1,4 @@
 $(document).ready(function() {
-    // prevent logo from obscuring navigation
-    $('.navbar-toggler-icon').click(function() {
-        console.log('navbar icon clicked');
-        $('.logo').toggle(); // should logo be shown or hidden
-    });
-
     // prevent jump to links from scrolling past headline
     window.addEventListener('hashchange', function() {
         window.scrollTo(window.scrollX, window.scrollY - 80);
@@ -13,6 +7,5 @@ $(document).ready(function() {
     // collapse navbar when user clicks on a nav link
     $('.nav-link').on('click',function() {
         $('.navbar-collapse').collapse('hide');
-        $('.logo').toggle();
     });
 });
